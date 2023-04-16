88cbdc45b5e581e90c4ee94fc3c7a95cc966e4d8
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 19:48:58 +0800

    Update crypto_gmail.sh

--- src/crypto_gmail.sh
+++ src/crypto_gmail.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 api="https://cryptogmail.com/api"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function get_domains() {
 	curl --request GET \
 		--url "$api/domains.config.json" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "accept: application/json" \
 		--header "content-type: application/json"
 }
@@ -13,7 +14,7 @@ function get_domains() {
 function get_inbox() {
 	curl --request GET \
 		--url "$api/emails?inbox=$1" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "accept: application/json" \
 		--header "content-type: application/json"
 }
@@ -21,7 +22,7 @@ function get_inbox() {
 function get_message_info() {
 	curl --request GET \
 		--url "$api/emails/$1" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "accept: application/json" \
 		--header "content-type: application/vnd.mailcare.v1+json"	
 }
